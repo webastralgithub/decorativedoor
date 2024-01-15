c224fbe76bce90bc1089fd4c9b553f72717dc38e
webastral <webastral.development@gmail.com>
2024-01-15 19:09:53 +0530

    new chnages

--- resources/views/frontend/category.blade.php
+++ resources/views/frontend/category.blade.php
@@ -57,12 +57,12 @@
                     @endif
                     </div>
                 </div>
-                <div class="product__pagination">
+                {{-- <div class="product__pagination">
                     <a href="#">1</a>
                     <a href="#">2</a>
                     <a href="#">3</a>
                     <a href="#"><i class="fa fa-long-arrow-right"></i></a>
-                </div>
+                </div> --}}
             </div>
         </div>
     </div>
