99c69288f69a465b6da86b9874bdb3b7d4c5a8ed
webastral <webastralgithub>
2024-01-23 11:07:02 +0530

    add filters

--- resources/views/admin/inventory/index.blade.php
+++ resources/views/admin/inventory/index.blade.php
@@ -4,20 +4,17 @@
 <div class="mx-4 content-p-mobile">
     <div class="page-header-tp">
         <h3>Inventory Information</h3>
-        <form >
-            <input type="search" class="form-control" placeholder="Find Inventry" name="q" value="{{ request('q') }}">
-        </form>
         <div class="top-bntspg-hdr">
             <a href="{{ route('inventory.create') }}" class="btn btn-primary btn-sm"> Add Inventory</a>
             <a href="{{ route('inventory.index') }}" class="btn btn-primary btn-sm">&larr; Back</a>
-        </div> 
+        </div>
     </div>
     @if(\Session::has('error'))
     <div>
         <li class="alert alert-danger">{!! \Session::get('error') !!}</li>
     </div>
     @endif
-    
+
     @if(\Session::has('success'))
     <div>
         <li class="alert alert-success">{!! \Session::get('success') !!}</li>
@@ -25,41 +22,50 @@
     @endif
     <div class="content-body">
         {{-- <div class="card-header">
-                        <h4 class="card-title">{{__('Statistics_by_Quantity_(TOP 15)')}}</h4>
-                    </div> --}}
-        <table class="table">
-            <thead>
-                <th>{{__('Product')}}</th>
-                <th>{{__('Inventory Quantity')}}</th>
-                <th>{{__('Order Quantity')}}</th>
-                <th>{{__('Total')}}</th>
-            </thead>
-            <tbody>
-                @foreach($products as $product)
-                @php
-                $inventoryquantity = 0;
-                $orderdetails = 0
-                @endphp
-                @foreach($product->inventories as $inventory)
+            <h4 class="card-title">{{__('Statistics_by_Quantity_(TOP 15)')}}</h4>
+        </div> --}}
+        <div class="table-order">
+            <table class="table table-bordered table-striped" id="inventory">
+                <thead>
+                    <th>{{__('Product')}}</th>
+                    <th>{{__('Inventory Quantity')}}</th>
+                    <th>{{__('Order Quantity')}}</th>
+                    <th>{{__('Total')}}</th>
+                </thead>
+                <tbody>
+                    @foreach($products as $product)
+                    @php
+                    $inventoryquantity = 0;
+                    $orderdetails = 0
+                    @endphp
+                    @foreach($product->inventories as $inventory)
                     @php
                     $inventoryquantity += $inventory->quantity;
                     @endphp
-                @endforeach
-                @foreach($product->orderdetails as $order)
-                @php
-                $orderdetails += $order->quantity;
-                @endphp
-            @endforeach
-                <tr>
-                    <td><a href="{{ route('inventory.show', $product->id) }}">{{ $product->title }}</a></td>
-                    <td>{{ $inventoryquantity }}</td>
-                    <td>{{ $orderdetails }}</td>
-                    <td>{{ abs($orderdetails - $inventoryquantity) }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-        {{ $products->links() }}
+                    @endforeach
+                    @foreach($product->orderdetails as $order)
+                    @php
+                    $orderdetails += $order->quantity;
+                    @endphp
+                    @endforeach
+                    <tr>
+                        <td><a href="{{ route('inventory.show', $product->id) }}">{{ $product->title }}</a></td>
+                        <td>{{ $inventoryquantity }}</td>
+                        <td>{{ $orderdetails }}</td>
+                        <td>{{ abs($orderdetails - $inventoryquantity) }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
+@endsection
+
+@section('scripts')
+<script>
+    new DataTable('#inventory', {
+        pageLength: 50
+    });
+</script>
 @endsection
--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -3,90 +3,98 @@
 @section('content')
 <div class="mx-4 content-p-mobile">
     <div class="page-header-tp">
-    <h3>Products List</h3>
-        <form >
-            <input type="search" class="form-control" placeholder="Find Products" name="q" value="{{ request('q') }}">
-        </form>
-    <div class="top-bntspg-hdr">
-        @can('create-product')
-        <a href="{{ route('products.create') }}" class="btn btn-primary btn-sm my-2"><i class="bi bi-plus-circle"></i> Add New Product</a>
-        @endcan
+        <h3>Products List</h3>
+        <div class="top-bntspg-hdr">
+            @can('create-product')
+            <a href="{{ route('products.create') }}" class="btn btn-primary btn-sm my-2"><i
+                    class="bi bi-plus-circle"></i> Add New Product</a>
+            @endcan
+        </div>
     </div>
-</div>
-@if(\Session::has('error'))
-<div>
-    <li class="alert alert-danger">{!! \Session::get('error') !!}</li>
-</div>
-@endif
+    @if(\Session::has('error'))
+    <div>
+        <li class="alert alert-danger">{!! \Session::get('error') !!}</li>
+    </div>
+    @endif
 
-@if(\Session::has('success'))
-<div>
-    <li class="alert alert-success">{!! \Session::get('success') !!}</li>
-</div>
-@endif
+    @if(\Session::has('success'))
+    <div>
+        <li class="alert alert-success">{!! \Session::get('success') !!}</li>
+    </div>
+    @endif
 
     <div class="content-body">
+        <div class="table-order">
+            <table class="table table-bordered table-striped" id="products">
+                <thead>
+                    <tr>
+                        <th scope="col">Action</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">On Hand Quantity</th>
+                        <th scope="col">On Order Quantity</th>
+                        <th scope="col">Available Quantity</th>
+                        <th scope="col">SKU</th>
+                        <th scope="col">Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($products as $product)
+                    <tr>
+                        <td>
+                            <div class="btn-group">
+                                <button type="button"
+                                    class="btn btn-primary btn-sm dropdown-toggle dropdown-toggle-split"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                    <span class="visually-hidden cogs-btn"><i class="fa fa-cog"
+                                            aria-hidden="true"></i></span>
+                                </button>
+                                <ul class="dropdown-menu">
+                                    <form action="{{ route('products.destroy', $product->id) }}" method="post">
+                                        @csrf
+                                        @method('DELETE')
 
-        <table class="table table-striped table-bordered">
-            <thead>
-                <tr>
-                    <th scope="col">Action</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">On Hand Quantity</th>
-                    <th scope="col">On Order Quantity</th>
-                    <th scope="col">Available Quantity</th>
-                    <th scope="col">SKU</th>
-                    <th scope="col">Price</th>
-                </tr>
-            </thead>
-            <tbody>
-                @forelse ($products as $product)
-                <tr>
-                    <td>
-                        <div class="btn-group">
-                            <button type="button" class="btn btn-primary btn-sm dropdown-toggle dropdown-toggle-split"
-                                data-bs-toggle="dropdown" aria-expanded="false">
-                                <span class="visually-hidden cogs-btn"><i class="fa fa-cog" aria-hidden="true"></i></span>
-                            </button>
-                            <ul class="dropdown-menu">
-                                <form action="{{ route('products.destroy', $product->id) }}" method="post">
-                                    @csrf
-                                    @method('DELETE')
-                    
-                                    @can('edit-product')
-                                    <li><a href="{{ route('products.edit', $product->id) }}" class="dropdown-item"><i
-                                                class="bi bi-pencil-square"></i> Edit</a></li>
-                                    @endcan
-                    
-                                    @can('delete-product')
-                                    <li><button type="submit" class="dropdown-item btn-danger"
-                                            onclick="return confirm('Do you want to delete this product?');"><i class="bi bi-trash"></i>
-                                            Delete</button></li>
-                                    @endcan
-                                </form>
-                            </ul>
-                        </div>
+                                        @can('edit-product')
+                                        <li><a href="{{ route('products.edit', $product->id) }}"
+                                                class="dropdown-item"><i class="bi bi-pencil-square"></i> Edit</a></li>
+                                        @endcan
+
+                                        @can('delete-product')
+                                        <li><button type="submit" class="dropdown-item btn-danger"
+                                                onclick="return confirm('Do you want to delete this product?');"><i
+                                                    class="bi bi-trash"></i>
+                                                Delete</button></li>
+                                        @endcan
+                                    </form>
+                                </ul>
+                            </div>
+                        </td>
+                        <td><a href="{{ route('products.edit', $product->id) }}">{{ $product->title }}</a>
+                        </td>
+                        <th>{{ getProductOnhandAvailabityStock($product->id) }}</th>
+                        <th>{{ getProductOnorderAvailabityStock($product->id) }}</th>
+                        <th>{{ getProductAvailabityStock($product->id) }}</th>
+                        <td>{{ $product->code }}</td>
+                        <td>${{ number_format($product->selling_price, 2, '.', ',') }}</td>
+                    </tr>
+                    @empty
+                    <td colspan="4">
+                        <span class="text-danger">
+                            <strong>No Product Found!</strong>
+                        </span>
                     </td>
-                    <td><a href="{{ route('products.edit', $product->id) }}">{{ $product->title }}</a>
-                    </td>
-                    <th>{{ getProductOnhandAvailabityStock($product->id) }}</th>
-                    <th>{{ getProductOnorderAvailabityStock($product->id) }}</th>
-                    <th>{{ getProductAvailabityStock($product->id) }}</th>
-                    <td>{{ $product->code }}</td>
-                    <td>${{ number_format($product->selling_price, 2, '.', ',') }}</td>
-                </tr>
-                @empty
-                <td colspan="4">
-                    <span class="text-danger">
-                        <strong>No Product Found!</strong>
-                    </span>
-                </td>
-                @endforelse
-            </tbody>
-        </table>
+                    @endforelse
+                </tbody>
+            </table>
 
-        {{ $products->links() }}
-
+        </div>
     </div>
 </div>
+@endsection
+
+@section('scripts')
+<script>
+    new DataTable('#products', {
+        pageLength: 50
+    });
+</script>
 @endsection
