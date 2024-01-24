3fd499c6f6be56b2dacf4977cb62702e87c3310c
webastral <webastral.development@gmail.com>
2024-01-24 17:22:26 +0530

    order listing

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,6 +46,6 @@ class User extends Authenticatable
 
     public function isAdmin()
     {
-        return $this->hasRole('Super Admin') || $this->hasRole('Admin') || $this->hasPermission('admin_access');
+        return $this->hasRole('Super Admin') || $this->hasRole('Admin') || $this->hasPermission('admin-access');
     }
 }
